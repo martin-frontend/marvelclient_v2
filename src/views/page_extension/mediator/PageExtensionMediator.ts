f402d0ed91a268a9182ea7a7c434572361279634
adam <adam.dong@baiyitech.com.tw>
2022-05-23 10:09:21 +0800

    移除 switch

--- src/views/page_extension/mediator/PageExtensionMediator.ts
+++ src/views/page_extension/mediator/PageExtensionMediator.ts
@@ -2,14 +2,13 @@ import AbstractMediator from "@/core/abstract/AbstractMediator";
 import PageExtensionProxy from "../proxy/PageExtensionProxy";
 import getProxy from "@/core/global/getProxy";
 
-export default class PageExtensionMediator extends AbstractMediator{
+export default class PageExtensionMediator extends AbstractMediator {
     public listNotificationInterests(): string[] {
         return [];
     }
 
     public handleNotification(notification: puremvc.INotification): void {
         const body = notification.getBody();
-        const myProxy:PageExtensionProxy = getProxy(PageExtensionProxy);
-        switch(notification.getName()){}
+        const myProxy: PageExtensionProxy = getProxy(PageExtensionProxy);
     }
 }

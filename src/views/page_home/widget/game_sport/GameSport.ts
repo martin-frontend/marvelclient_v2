34b88c7157c67cc8fcf913f4a402e7b4ce350526
custer <custer@xx.xx>
2022-05-25 21:52:30 +0700

    优化

--- src/views/page_home/widget/game_sport/GameSport.ts
+++ src/views/page_home/widget/game_sport/GameSport.ts
@@ -1,5 +1,32 @@
+import Assets from "@/assets/Assets";
 import AbstractView from "@/core/abstract/AbstractView";
+import GameProxy from "@/proxy/GameProxy";
+import page_game_list from "@/views/page_game_list";
 import { Prop, Watch, Component } from "vue-property-decorator";
 
 @Component
-export default class GameSport extends AbstractView {}
+export default class GameSport extends AbstractView {
+    CategoryIcon = Assets.CategoryIcon;
+    @Prop() data!: any;
+
+    getIcon(item: any) {
+        if (item.icon.indexOf("http") != -1) {
+            return item.icon;
+        } else {
+            if (item.list_type == 0) {
+                return `img/productimage/${item.icon}`;
+            } else {
+                return `img/changlogo/${item.icon}`;
+            }
+        }
+    }
+
+    goGamePlay(item:any) {
+        const gameProxy: GameProxy = this.getProxy(GameProxy);
+        gameProxy.api_vendor_var_ori_product_show_var(item);
+    }
+
+    onShowAll() {
+        page_game_list.show();
+    }
+}

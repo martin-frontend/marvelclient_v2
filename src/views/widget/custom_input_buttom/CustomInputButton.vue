4be634406453518b3d434d739512858da785172c
adam <adam.dong@baiyitech.com.tw>
2022-05-25 14:10:27 +0800

    新增 widget CustomInputButton

---
+++ src/views/widget/custom_input_buttom/CustomInputButton.vue
@@ -0,0 +1,10 @@
+<template src="./CustomInputButton.vue.html" lang="html"></template>
+<style src="./CustomInputButton.vue.scss" lang="scss" scoped></style>
+
+<script lang="ts">
+import Component from "vue-class-component";
+import CustomInputButton from "./CustomInputButton";
+
+@Component
+export default class extends CustomInputButton {}
+</script>

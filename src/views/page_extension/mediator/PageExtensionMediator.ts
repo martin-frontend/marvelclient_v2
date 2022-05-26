53972af546b3011d225018878312222acb72446b
adam <adam.dong@baiyitech.com.tw>
2022-05-26 13:00:52 +0800

    更新推广赚钱

--- src/views/page_extension/mediator/PageExtensionMediator.ts
+++ src/views/page_extension/mediator/PageExtensionMediator.ts
@@ -3,13 +3,24 @@ import PageExtensionProxy from "../proxy/PageExtensionProxy";
 import getProxy from "@/core/global/getProxy";
 
 export default class PageExtensionMediator extends AbstractMediator {
+    private myProxy: PageExtensionProxy = this.getProxy(PageExtensionProxy);
     public listNotificationInterests(): string[] {
-        return [];
+        return [
+            net.EventType.api_user_var_commission_commissiondetail,
+            net.EventType.api_user_var_commission_commissionnum,
+        ];
     }
 
     public handleNotification(notification: puremvc.INotification): void {
         const body = notification.getBody();
         const myProxy: PageExtensionProxy = getProxy(PageExtensionProxy);
-        // switch(notification.getName()){}
+        switch (notification.getName()) {
+            case net.EventType.api_user_var_commission_commissiondetail:
+                this.myProxy.setData(body);
+                break;
+            case net.EventType.api_user_var_commission_commissionnum:
+                this.myProxy.setCommissionCommissionnum(body);
+                break;
+        }
     }
 }

ba936dd4fcd4bcc3fd463aa42aeb273f6dd3c7ab
Anders <anders.nat@icloud.com>
2022-05-26 14:09:37 +0800

    直属成员 API

--- src/views/dialog_directly/views/DialogDirectly.ts
+++ src/views/dialog_directly/views/DialogDirectly.ts
@@ -1,21 +1,27 @@
+import Assets from "@/assets/Assets";
 import AbstractView from "@/core/abstract/AbstractView";
 import BlurUtil from "@/core/global/BlurUtil";
 import CopyUtil from "@/core/global/CopyUtil";
 import { Watch, Component } from "vue-property-decorator";
 import DialogDirectlyMediator from "../mediator/DialogDirectlyMediator";
 import DialogDirectlyProxy from "../proxy/DialogDirectlyProxy";
+import DialogPromotionFloorProxy from "@/views/dialog_promotion_floor/proxy/DialogPromotionFloorProxy";
 import dialog_promotion_floor from "@/views/dialog_promotion_floor";
 
 @Component
 export default class DialogDirectly extends AbstractView {
+    dialogPromotionFloorProxy: DialogPromotionFloorProxy = this.getProxy(DialogPromotionFloorProxy);
     myProxy: DialogDirectlyProxy = this.getProxy(DialogDirectlyProxy);
     pageData = this.myProxy.pageData;
+
+    commonIcon = Assets.commonIcon;
 
     constructor() {
         super(DialogDirectlyMediator);
     }
 
-    handlerSetting() {
+    handlerSetting(data: any) {
+        this.dialogPromotionFloorProxy.setSelectedFloorData(data);
         dialog_promotion_floor.show();
     }
 
@@ -30,10 +36,17 @@ export default class DialogDirectly extends AbstractView {
             //如果是列表，使用以下数据，否则删除
             // this.myProxy.resetQuery();
             // this.myProxy.api_xxx();
+            this.myProxy.api_user_var_agent_direct_list();
         }
     }
 
     search() {
-        console.log('click search');
+        this.myProxy.pageData.list = [];
+        this.myProxy.parameter.direct_user_id = this.myProxy.pageData.search;
+        this.myProxy.api_user_var_agent_direct_list();
+    }
+
+    onPageChange():void {
+        this.myProxy.api_user_var_agent_direct_list();
     }
 }
--- src/views/dialog_promotion_floor/views/DialogPromotionFloor.ts
+++ src/views/dialog_promotion_floor/views/DialogPromotionFloor.ts
@@ -1,12 +1,14 @@
 import AbstractView from "@/core/abstract/AbstractView";
 import BlurUtil from "@/core/global/BlurUtil";
 import CopyUtil from "@/core/global/CopyUtil";
+import DialogDirectlyProxy from "@/views/dialog_directly/proxy/DialogDirectlyProxy";
 import { Watch, Component } from "vue-property-decorator";
 import DialogPromotionFloorMediator from "../mediator/DialogPromotionFloorMediator";
 import DialogPromotionFloorProxy from "../proxy/DialogPromotionFloorProxy";
 
 @Component
 export default class DialogPromotionFloor extends AbstractView {
+    private dialogDirectlyProxy: DialogDirectlyProxy = this.getProxy(DialogDirectlyProxy);
     myProxy: DialogPromotionFloorProxy = this.getProxy(DialogPromotionFloorProxy);
     pageData = this.myProxy.pageData;
 
@@ -30,10 +32,14 @@ export default class DialogPromotionFloor extends AbstractView {
 
     get isCheckedAmount(): boolean {
         const { amount } = this.pageData;
-        return (this.checkAmount(amount))
+        return this.checkAmount(amount);
     }
 
     private checkAmount(id: any) {
-        return id.length > 0 ? true : false
+        return id.length > 0 ? true : false;
+    }
+
+    setFloor(): void {
+        this.myProxy.api_user_var_agent_var_update();
     }
 }

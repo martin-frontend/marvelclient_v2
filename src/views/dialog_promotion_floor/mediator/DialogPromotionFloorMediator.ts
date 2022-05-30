54c3211646035bf48aad294b247aba16aa4c104d
Anders <anders.nat@icloud.com>
2022-05-30 14:43:54 +0800

    样式调整

--- src/views/dialog_promotion_floor/mediator/DialogPromotionFloorMediator.ts
+++ src/views/dialog_promotion_floor/mediator/DialogPromotionFloorMediator.ts
@@ -1,6 +1,7 @@
 import AbstractMediator from "@/core/abstract/AbstractMediator";
 import DialogPromotionFloorProxy from "../proxy/DialogPromotionFloorProxy";
 import getProxy from "@/core/global/getProxy";
+import DialogDirectlyProxy from "@/views/dialog_directly/proxy/DialogDirectlyProxy";
 
 export default class DialogPromotionFloorMediator extends AbstractMediator {
     public listNotificationInterests(): string[] {
@@ -9,10 +10,12 @@ export default class DialogPromotionFloorMediator extends AbstractMediator {
 
     public handleNotification(notification: puremvc.INotification): void {
         const body = notification.getBody();
+        const dialogDirectlyProxy: DialogDirectlyProxy = getProxy(DialogDirectlyProxy);
         const myProxy: DialogPromotionFloorProxy = getProxy(DialogPromotionFloorProxy);
         switch (notification.getName()) {
             case net.EventType.api_user_var_agent_var_update:
                 myProxy.setData(body);
+                dialogDirectlyProxy.api_user_var_agent_direct_list();
                 break;
         }
     }

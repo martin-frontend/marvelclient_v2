5813de1634764e72482c0bf94c36a7fe8c93cc1e
adam <adam.dong@baiyitech.com.tw>
2022-05-31 12:01:26 +0800

    更新分红页面

--- src/views/page_bonus/views/PageBonus.ts
+++ src/views/page_bonus/views/PageBonus.ts
@@ -2,6 +2,10 @@ import AbstractView from "@/core/abstract/AbstractView";
 import { Watch, Component } from "vue-property-decorator";
 import PageBonusMediator from "../mediator/PageBonusMediator";
 import PageBonusProxy from "../proxy/PageBonusProxy";
+import dialog_pledge from "@/views/dialog_pledge";
+import dialog_manually_unstaking from "@/views/dialog_manually_unstaking";
+import dialog_pledge_records from "@/views/dialog_pledge_records";
+import dialog_wallet from "@/views/dialog_wallet";
 
 @Component
 export default class PageBonus extends AbstractView {
@@ -10,5 +14,33 @@ export default class PageBonus extends AbstractView {
 
     constructor() {
         super(PageBonusMediator);
+    }
+
+    destroyed() {
+        super.destroyed();
+    }
+
+    handlerPledge() {
+        dialog_pledge.show();
+    }
+
+    handlerUnpledge() {
+        dialog_manually_unstaking.show();
+    }
+
+    handlerRecords() {
+        dialog_pledge_records.show();
+    }
+
+    jumpTo(target: string) {
+        window.scrollTo({
+            //@ts-ignore
+            top: document.querySelector(target).offsetTop,
+            behavior: "smooth",
+        });
+    }
+
+    handleRecords() {
+        dialog_wallet.show(2);
     }
 }

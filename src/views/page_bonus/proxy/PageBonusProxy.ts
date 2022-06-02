16ae6e62854e8d8aef9c3b83ed1c22d9871b2bfe
adam <adam.dong@baiyitech.com.tw>
2022-06-02 12:30:02 +0800

    更新分红 API 数据

--- src/views/page_bonus/proxy/PageBonusProxy.ts
+++ src/views/page_bonus/proxy/PageBonusProxy.ts
@@ -27,9 +27,31 @@ export default class PageBonusProxy extends puremvc.Proxy {
         total_bonus_amount: "",         // 总计已派发
         total_stake_amount: "",         // 总质押数量
     };
+    user_stake_info: any = {
+        amount: "",                    // 质押币余额
+        stake_amount: "",              // 个人质押
+        stake_ratio: "",               // 占比
+        stake_bonus_awaiting_num: "",  // 可领取分红
+        stake_bonus_received_num: ""   // 个人累计分红
+    }
+    bonus_rank: any = [];
+    bonus_recently: any = [];
 
     setPlatData(data: any) {
         Object.assign(this.plat_stake_info, data);
+    }
+
+    setUserData(data: any) {
+        Object.assign(this.user_stake_info, data);
+    }
+
+    setBonusRank(data: any) {
+        this.bonus_rank = data;
+    }
+
+    setBonusRecently(data: any) {
+        this.bonus_recently = data;
+        console.log(this.bonus_recently);
     }
 
     /**--分红--平台币分红信息*/
@@ -40,5 +62,15 @@ export default class PageBonusProxy extends puremvc.Proxy {
     /**--分红--平台币分红信息*/
     api_user_var_stake_info() {
         this.sendNotification(net.HttpType.api_user_var_stake_info, { user_id: core.user_id });
+    }
+
+    /**--分红--平台近5日分红金额列表*/
+    api_plat_var_bonus_recently() {
+        this.sendNotification(net.HttpType.api_plat_var_bonus_recently, { plat_id: core.plat_id });
+    }
+
+    /**--分红--昨日分红排行榜*/
+    api_plat_var_bonus_rank() {
+        this.sendNotification(net.HttpType.api_plat_var_bonus_rank, { plat_id: core.plat_id });
     }
 }
--- src/views/page_bonus/views/PageBonus.ts
+++ src/views/page_bonus/views/PageBonus.ts
@@ -13,7 +13,15 @@ export default class PageBonus extends AbstractView {
     myProxy: PageBonusProxy = this.getProxy(PageBonusProxy);
     pageData = this.myProxy.pageData;
     plat_stake_info = this.myProxy.plat_stake_info;
+    user_stake_info = this.myProxy.user_stake_info;
     listQuery = this.pageData.listQuery;
+    get bonus_rank() {
+        return this.myProxy.bonus_rank;
+    }
+
+    get bonus_recently() {
+        return this.myProxy.bonus_recently;
+    }
 
     constructor() {
         super(PageBonusMediator);
@@ -50,5 +58,16 @@ export default class PageBonus extends AbstractView {
     onTabClick(cate: number) {
         this.listQuery.cate = cate;
         this.listQuery.page_count = 1;
+    }
+
+    getDateTime(data: any) {
+        // 2022-05-25 18:51:10
+        const md = `${data.split(" ")[0].split("-")[1]}-${data.split(" ")[0].split("-")[2]}`;
+        return `${md}`;
+    }
+
+    getHeight() {
+        const maxBonus = Math.max(...this.bonus_recently.map((o: { y: any; }) => o.y))
+        const height = "$vuetify.breakpoint.xsOnly? 140 : 280"
     }
 }

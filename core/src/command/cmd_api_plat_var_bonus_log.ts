20bdc5a4a863028fd74828263bd77c7de73a0d32
adam <adam.dong@baiyitech.com.tw>
2022-06-03 13:06:52 +0800

    新增分红 API

---
+++ core/src/command/cmd_api_plat_var_bonus_log.ts
@@ -0,0 +1,18 @@
+/**
+ * 分红记录-全站记录
+ */
+module net {
+    export class cmd_api_plat_var_bonus_log extends puremvc.SimpleCommand {
+        execute(notification: puremvc.INotification) {
+            const body = notification.getBody() || {};
+            const url = getUrl(HttpType.api_plat_var_bonus_log, body);
+            Http.request(body || {}, url).then(this.response.bind(this));
+        }
+
+        private response(result: core.ResponseVO) {
+            if (result.status === 0) {
+                this.sendNotification(EventType.api_plat_var_bonus_log, result.data);
+            }
+        }
+    }
+}

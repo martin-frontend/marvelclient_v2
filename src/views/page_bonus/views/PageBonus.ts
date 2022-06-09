eb2b25834d1cdfafe55e7bad0308d8525fad2125
adam <adam.dong@baiyitech.com.tw>
2022-06-09 11:41:07 +0800

    调整多语系

--- src/views/page_bonus/views/PageBonus.ts
+++ src/views/page_bonus/views/PageBonus.ts
@@ -9,6 +9,7 @@ import dialog_pledge_records from "@/views/dialog_pledge_records";
 import dialog_wallet from "@/views/dialog_wallet";
 import dialog_message_box from "@/views/dialog_message_box";
 import GamePlatConfig from "@/core/config/GamePlatConfig";
+import LangUtil from "@/core/global/LangUtil";
 
 @Component
 export default class PageBonus extends AbstractView {
@@ -19,6 +20,7 @@ export default class PageBonus extends AbstractView {
     listQuery = this.pageData.listQuery;
     questionData = this.myProxy.questionData;
     GamePlatConfig = GamePlatConfig;
+    LangUtil = LangUtil;
 
     commonIcon = Assets.commonIcon;
 

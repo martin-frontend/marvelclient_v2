cdb98a09f1afca0c47f1554f90c4aa30b25d09c9
custer <custer@xx.xx>
2022-06-13 17:31:47 +0700

    gen

--- build/gen_dialog.js
+++ build/gen_dialog.js
@@ -7,6 +7,9 @@ const srcPath = path.resolve(__dirname, "../src");
 
 const moduleName = process.argv[2];
 if (!moduleName) throw "参数错误";
+if(process.argv[3]){
+    srcPath = path.resolve(srcPath, process.argv[3]);
+}
 const modulePath = path.resolve(srcPath, "views", moduleName);
 const className = utils.getClassName(moduleName);
 (function () {

48082ba774df3c970aa29ab1469aad1882a1e284
adam <adam.dong@baiyitech.com.tw>
2022-06-17 14:17:52 +0800

    重构 dialog_wallet

--- src/views/dialog_wallet/widget/tab_account_detail/TabAccountDetail.vue
+++ src/views/dialog_wallet/widget/tab_account_detail/TabAccountDetail.vue
@@ -5,10 +5,12 @@
 import CustomSelect from "@/views/widget/custom_select/CustomSelect.vue";
 import Component from "vue-class-component";
 import TabAccountDetail from "./TabAccountDetail";
+import ListNodata from "@/views/widget/list_nodata/ListNodata.vue";
 
 @Component({
     components: {
         CustomSelect,
+        ListNodata,
     },
 })
 export default class extends TabAccountDetail {}

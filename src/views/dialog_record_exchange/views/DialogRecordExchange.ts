b511650ec8b1f117df1191c135e414b34440c886
adam <adam.dong@baiyitech.com.tw>
2022-06-18 16:20:31 +0800

    重构 dialog_record_exchange

--- src/views/dialog_record_exchange/views/DialogRecordExchange.ts
+++ src/views/dialog_record_exchange/views/DialogRecordExchange.ts
@@ -8,16 +8,12 @@ import Component from "vue-class-component";
 import { Watch } from "vue-property-decorator";
 import DialogRecordExchangeMediator from "../mediator/DialogRecordExchangeMediator";
 import DialogRecordExchangeProxy from "../proxy/DialogRecordExchangeProxy";
-import { handleScroll } from "@/core/global/Functions";
-import GlobalVar from "@/core/global/GlobalVar";
 
 @Component
 export default class DialogRecordExchange extends AbstractView {
     LangUtil = LangUtil;
-    handleScroll = handleScroll;
     myProxy: DialogRecordExchangeProxy = this.getProxy(DialogRecordExchangeProxy);
     pageData = this.myProxy.pageData;
-    scrollStatus = GlobalVar.scrollStatus;
 
     commonIcon = Assets.commonIcon;
 
@@ -42,39 +38,20 @@ export default class DialogRecordExchange extends AbstractView {
         if (this.pageData.bShow) {
             this.myProxy.resetQuery();
             this.myProxy.api_user_var_exchange_order_list();
-            this.myProxy.pageData.isMobile = this.$vuetify.breakpoint.width < 600;
-        }
-    }
-
-    @Watch("pageData.list.length")
-    onWatchList() {
-        if (this.pageData.list.length > 0) {
-            this.handlerScroll();
-        }
-    }
-
-    // 监听手机版scroll 到底加载
-    @Watch("scrollStatus.flag")
-    onScroll() {
-        if (this.myProxy.pageData.pageInfo.pageCurrent < this.myProxy.pageData.pageInfo.pageCount) {
-            this.myProxy.pageData.listQuery.page_count++;
-            this.myProxy.api_user_var_exchange_order_list();
-        }
-    }
-
-    handlerScroll() {
-        if (this.$vuetify.breakpoint.xsOnly) {
-            this.$nextTick(() => {
-                GlobalVar.HTMLElement.dom = document.querySelector(".table_data") as HTMLElement;
-                GlobalVar.HTMLElement.dom.removeEventListener("scroll", this.handleScroll);
-                GlobalVar.HTMLElement.dom.addEventListener("scroll", this.handleScroll);
-            });
         }
     }
 
     onPageChange(val: any) {
         this.pageData.listQuery.page_count = val;
         this.myProxy.api_user_var_exchange_order_list();
+    }
+
+    onRefresh(done: any) {
+        this.myProxy.listRefrush(done);
+    }
+
+    onLoad(done: any) {
+        this.myProxy.listMore(done);
     }
 
     get listHeight() {

5f0e01ae471e7ddc8bad278c1db7e904c4da3001
custer <custer@xx.xx>
2022-06-25 15:37:59 +0700

    ios禁用dialog时body的滚动

--- src/core/global/BlurUtil.ts
+++ src/core/global/BlurUtil.ts
@@ -2,5 +2,16 @@ export default function (blur: boolean) {
     const pageDiv = document.getElementById("page");
     if (pageDiv) {
         pageDiv.style.filter = blur ? "blur(6px)" : "none";
+        if(blur){
+            document.documentElement.style.overflow = "hidden";
+            document.documentElement.style.position = "fixed";
+            //@ts-ignore
+            document.body.scroll = "no";
+        }else{
+            document.documentElement.style.overflow = "scroll";
+            document.documentElement.style.position = "relative";
+            //@ts-ignore
+            document.body.scroll = "yes";
+        }
     }
 }

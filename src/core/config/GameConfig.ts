075a6602373e5dffcb40c313f826ecf209e6f49d
custer <custer@xx.xx>
2022-06-27 10:33:14 +0700

    bug修复

--- src/core/config/GameConfig.ts
+++ src/core/config/GameConfig.ts
@@ -32,7 +32,7 @@ export default class GameConfig {
 
         if (core.plat_id) {
             const fileName: string = new core.MD5().hex_md5("plat-" + core.plat_id);
-            const url = `${core.cdnUrl}/client_config/${fileName}.json?${Math.random()}`;
+            const url = `${core.cdnUrl}/resource/client_config/${fileName}.json?${Math.random()}`;
             axios
                 .get(url)
                 .then((response: any) => {
--- src/core/global/BlurUtil.ts
+++ src/core/global/BlurUtil.ts
@@ -4,12 +4,12 @@ export default function (blur: boolean) {
         pageDiv.style.filter = blur ? "blur(6px)" : "none";
         if(blur){
             document.documentElement.style.overflow = "hidden";
-            document.documentElement.style.position = "fixed";
+            // document.documentElement.style.position = "fixed";
             //@ts-ignore
             document.body.scroll = "no";
         }else{
             document.documentElement.style.overflow = "scroll";
-            document.documentElement.style.position = "relative";
+            // document.documentElement.style.position = "relative";
             //@ts-ignore
             document.body.scroll = "yes";
         }

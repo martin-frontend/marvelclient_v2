52c86b5b668682f241b30f9d9fab46f9fc3c5eb2
adam <adam.dong@baiyitech.com.tw>
2022-07-01 11:58:38 +0800

    新增 Swap API

---
+++ core/src/command/cmd_api_plat_var_swap_setting_info.ts
@@ -0,0 +1,18 @@
+/**
+ * Swap基础信息
+ */
+module net {
+    export class cmd_api_plat_var_swap_setting_info extends puremvc.SimpleCommand {
+        execute(notification: puremvc.INotification) {
+            const body = notification.getBody() || {};
+            const url = getUrl(HttpType.api_plat_var_swap_setting_info, body);
+            Http.request(body || {}, url).then(this.response.bind(this));
+        }
+
+        private response(result: core.ResponseVO) {
+            if (result.status === 0) {
+                this.sendNotification(EventType.api_plat_var_swap_setting_info, result.data);
+            }
+        }
+    }
+}

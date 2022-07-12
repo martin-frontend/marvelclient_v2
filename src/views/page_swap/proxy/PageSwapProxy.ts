52983d109ef1c675e4f598d75ac68a7e03267e79
adam <adam.dong@baiyitech.com.tw>
2022-07-12 10:17:53 +0800

    修正 bug

--- src/views/page_swap/proxy/PageSwapProxy.ts
+++ src/views/page_swap/proxy/PageSwapProxy.ts
@@ -28,7 +28,7 @@ export default class PageSwapProxy extends puremvc.Proxy {
             inputB: "",
             coinA: "",
             coinB: "",
-            timestamp: 0,
+            timestamp: 1,
             price: "",
 
             min_to_coin_number: "", // 最小获取量(用于显示可以兑换出的数量)
@@ -114,7 +114,7 @@ export default class PageSwapProxy extends puremvc.Proxy {
     setData(data: any) {
         Object.assign(this.pageData.swap_setting_info, data);
         const { form, chartData } = this.pageData;
-        form.tolerance = data.tolerance_params[0];
+        // form.tolerance = data.tolerance_params[0];
         form.coinA = data.coin_a;
         form.coinB = data.coin_b;
         form.type = 0;
@@ -133,7 +133,7 @@ export default class PageSwapProxy extends puremvc.Proxy {
 
         if (data.timestamp == timestamp) {
             //第一次试算不填值
-            if (timestamp > 1) {
+            if (Number(timestamp) > 1) {
                 if (inputType == 0) {
                     form.inputB = data.to_coin_number;
                 } else {
@@ -275,11 +275,23 @@ export default class PageSwapProxy extends puremvc.Proxy {
 
     /**Swap--Swap试算*/
     api_plat_var_swap_trial() {
+
+
         const { inputType, inputA, inputB, coinA, coinB, tolerance, timestamp } = this.pageData.form;
+        let coin_number = "0";
+        if (timestamp != 1) {
+            if (inputType == 0) {
+                coin_number = inputA
+            } else {
+                coin_number = inputB
+            }
+        } else {
+            coin_number = "1";
+        }
         const data = {
             plat_id: core.plat_id,
             from_coin: inputType == 0 ? coinA : coinB,
-            from_coin_number: inputType == 0 ? inputA : inputB,
+            from_coin_number: coin_number,
             tolerance,
             timestamp,
         };

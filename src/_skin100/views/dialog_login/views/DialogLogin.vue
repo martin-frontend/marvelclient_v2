8da9973cc77c024c2b950356625371e5d14880cf
pangpang <afanso86@yandex.com>
2022-07-14 15:26:07 +0400

    1.安全中心换肤完成

--- src/_skin100/views/dialog_login/views/DialogLogin.vue
+++ src/_skin100/views/dialog_login/views/DialogLogin.vue
@@ -3,8 +3,8 @@
 
 <script lang="ts">
 import AdvertiseInLogin from "@/views/widget/advertise_in_login/AdvertiseInLogin.vue";
-import CustomInput from "@/views/widget/custom_input/CustomInput.vue";
-import CustomPassword from "@/views/widget/custom_password/CustomPassword.vue";
+import CustomInput from "@/_skin100/views/widget/custom_input/CustomInput.vue";
+import CustomPassword from "@/_skin100/views/widget/custom_password/CustomPassword.vue";
 import Overlay from "@/views/widget/overlay/Overlay.vue";
 import Component from "vue-class-component";
 import DialogLogin from "./DialogLogin";
--- src/_skin100/views/dialog_safety_center/views/DialogSafetyCenter.vue
+++ src/_skin100/views/dialog_safety_center/views/DialogSafetyCenter.vue
@@ -1,13 +1,13 @@
-<template src="./DialogRegister.vue.html" lang="html"></template>
-<style src="./DialogRegister.vue.scss" lang="scss" scoped></style>
+<template src="./DialogSafetyCenter.vue.html" lang="html"></template>
+<style src="./DialogSafetyCenter.vue.scss" lang="scss" scoped></style>
 
 <script lang="ts">
+import Component from "vue-class-component";
+import Overlay from "@/views/widget/overlay/Overlay.vue";
+import DialogSafetyCenter from "./DialogSafetyCenter";
 import AdvertiseInLogin from "@/views/widget/advertise_in_login/AdvertiseInLogin.vue";
 import CustomInput from "@/_skin100/views/widget/custom_input/CustomInput.vue";
 import CustomPassword from "@/_skin100/views/widget/custom_password/CustomPassword.vue";
-import Overlay from "@/views/widget/overlay/Overlay.vue";
-import Component from "vue-class-component";
-import DialogRegister from "./DialogRegister";
 
 @Component({
     components: {
@@ -17,5 +17,5 @@ import DialogRegister from "./DialogRegister";
         Overlay,
     },
 })
-export default class extends DialogRegister {}
+export default class extends DialogSafetyCenter {}
 </script>

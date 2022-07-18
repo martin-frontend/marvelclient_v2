501b77e8c9fbbe1f67aed66d043e8dc84a699fdd
pangpang <afanso86@yandex.com>
2022-07-19 00:07:36 +0400

    1.充值兑换ing

---
+++ src/_skin100/views/dialog_record_recharge/views/DialogRecordRecharge.ts
@@ -0,0 +1,65 @@
+import Assets from "@/assets/Assets";
+import AbstractView from "@/core/abstract/AbstractView";
+import BlurUtil from "@/core/global/BlurUtil";
+import CopyUtil from "@/core/global/CopyUtil";
+import LangUtil from "@/core/global/LangUtil";
+import dialog_recharge from "@/_skin100/views/dialog_recharge";
+import Component from "vue-class-component";
+import { Watch } from "vue-property-decorator";
+import DialogRecordRechargeMediator from "../mediator/DialogRecordRechargeMediator";
+import DialogRecordRechargeProxy from "../proxy/DialogRecordRechargeProxy";
+
+@Component
+export default class DialogRecordRecharge extends AbstractView {
+    LangUtil = LangUtil;
+    myProxy: DialogRecordRechargeProxy = this.getProxy(DialogRecordRechargeProxy);
+    pageData = this.myProxy.pageData;
+    statusOptions = this.myProxy.statusOptions;
+
+    commonIcon = Assets.commonIcon;
+
+    constructor() {
+        super(DialogRecordRechargeMediator);
+    }
+
+    onCopy(value: string) {
+        CopyUtil(value);
+    }
+
+    onClose() {
+        this.pageData.bShow = false;
+        setTimeout(() => {
+            dialog_recharge.show();
+        }, 100);
+    }
+
+    @Watch("pageData.bShow")
+    onWatchShow() {
+        BlurUtil(this.pageData.bShow);
+        if (this.pageData.bShow) {
+            this.myProxy.resetQuery();
+            this.myProxy.api_user_var_recharge_list();
+        }
+    }
+
+    @Watch("$vuetify.breakpoint.xsOnly")
+    onWatchXS() {
+        if (this.pageData.bShow) {
+            this.pageData.listQuery.page_count = 1;
+            this.myProxy.api_user_var_recharge_list();
+        }
+    }
+
+    onPageChange(val: any) {
+        this.pageData.listQuery.page_count = val;
+        this.myProxy.api_user_var_recharge_list();
+    }
+
+    onRefresh(done: any) {
+        this.myProxy.listRefrush(done);
+    }
+
+    onLoad(done: any) {
+        this.myProxy.listMore(done);
+    }
+}
---
+++ src/_skin100/views/dialog_record_recharge/views/DialogRecordRecharge.vue
@@ -0,0 +1,17 @@
+<template src="./DialogRecordRecharge.vue.html" lang="html"></template>
+<style src="./DialogRecordRecharge.vue.scss" lang="scss" scoped></style>
+
+<script lang="ts">
+import Overlay from "@/views/widget/overlay/Overlay.vue";
+import Component from "vue-class-component";
+import DialogRecordRecharge from "./DialogRecordRecharge";
+import ListNodata from "@/views/widget/list_nodata/ListNodata.vue";
+
+@Component({
+    components: {
+        Overlay,
+        ListNodata,
+    },
+})
+export default class extends DialogRecordRecharge {}
+</script>

48fd66a84aa12396351e2e9a34f80f5f091180de
pangpang <afanso86@yandex.com>
2022-07-20 11:06:36 +0400

    1,提币记录

--- src/_skin100/views/dialog_record_exchange/views/DialogRecordExchange.ts
+++ src/_skin100/views/dialog_record_exchange/views/DialogRecordExchange.ts
@@ -3,6 +3,7 @@ import AbstractView from "@/core/abstract/AbstractView";
 import BlurUtil from "@/core/global/BlurUtil";
 import CopyUtil from "@/core/global/CopyUtil";
 import LangUtil from "@/core/global/LangUtil";
+import GamePlatConfig from "@/core/config/GamePlatConfig";
 import dialog_recharge from "@/_skin100/views/dialog_recharge";
 import DialogRechargeProxy from "@/views/dialog_recharge/proxy/DialogRechargeProxy";
 import Component from "vue-class-component";
@@ -17,6 +18,7 @@ export default class DialogRecordExchange extends AbstractView {
     rechargeProxy: DialogRechargeProxy = this.getProxy(DialogRechargeProxy);
     pageData = this.myProxy.pageData;
 
+    plat_coins = GamePlatConfig.config.plat_coins;
     commonIcon = Assets.commonIcon;
 
     constructor() {
--- src/_skin100/views/dialog_record_exchange/views/DialogRecordExchange.vue
+++ src/_skin100/views/dialog_record_exchange/views/DialogRecordExchange.vue
@@ -5,7 +5,7 @@
 import Overlay from "@/views/widget/overlay/Overlay.vue";
 import Component from "vue-class-component";
 import DialogRecordExchange from "./DialogRecordExchange";
-import ListNodata from "@/views/widget/list_nodata/ListNodata.vue";
+import ListNodata from "@/_skin100/views/widget/list_nodata/ListNodata.vue";
 
 @Component({
     components: {

269c358e1c0c8fafb29ec85efc337f4dfb77b47c
custer <custer@xx.xx>
2022-07-22 22:07:44 +0700

    openlink

--- src/core/global/OpenLink.ts
+++ src/core/global/OpenLink.ts
@@ -4,15 +4,15 @@ export default function OpenLink(url: string) {
     if (core.app_type == core.EnumAppType.APP) {
         WebViewBridge.getInstance().openStstemBrowser(url);
     } else {
-        try {
-            const winHandler: any = window.open("", "_blank");
-            winHandler.location.href = url;
-        } catch (e) {
-            const a = document.createElement("a"); //创建a标签
-            a.setAttribute("href", url);
-            a.setAttribute("target", "_blank");
-            document.body.appendChild(a);
-            a.click(); //执行当前对象
-        }
+        // try {
+        //     const winHandler: any = window.open("", "_blank");
+        //     winHandler.location.href = url;
+        // } catch (e) {
+        const a = document.createElement("a"); //创建a标签
+        a.setAttribute("href", url);
+        a.setAttribute("target", "_blank");
+        document.body.appendChild(a);
+        a.click(); //执行当前对象
+        // }
     }
 }

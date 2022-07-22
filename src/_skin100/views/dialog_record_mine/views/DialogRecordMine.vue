4065086bb99bd75c8e0ec8659b9111960d994cf2
samuel <samuel@samuel.com>
2022-07-22 21:33:14 +0400

    分红 dialog

---
+++ src/_skin100/views/dialog_record_mine/views/DialogRecordMine.vue
@@ -0,0 +1,16 @@
+<template src="./DialogRecordMine.vue.html" lang="html"></template>
+<style src="./DialogRecordMine.vue.scss" lang="scss" scoped></style>
+
+<script lang="ts">
+import Component from "vue-class-component";
+import Overlay from "@/views/widget/overlay/Overlay.vue";
+import DialogRecordMine from "./DialogRecordMine";
+import ListNodata from "@/_skin100/views/widget/list_nodata/ListNodata.vue";
+@Component({
+    components: {
+        Overlay,
+        ListNodata,
+    },
+})
+export default class extends DialogRecordMine {}
+</script>

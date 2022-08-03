b7f163d1ddc986d30448cb34f0d23a3c25baaeb6
martin <Martin.yu@baiyitech.com.tw>
2022-08-03 12:55:32 +0800

    调整币种提示动画
    (cherry picked from commit 503ccc74f191cabbe2acc978d9704b71e3599f5b)

--- src/views/header/widget/wallet/coin_tip/CoinTip.ts
+++ src/views/header/widget/wallet/coin_tip/CoinTip.ts
@@ -1,4 +1,7 @@
 import AbstractView from "@/core/abstract/AbstractView";
 import { Component } from "vue-property-decorator";
+import LangUtil from "@/core/global/LangUtil";
 @Component
-export default class CoinTip extends AbstractView {}
+export default class CoinTip extends AbstractView {
+    LangUtil = LangUtil;
+}

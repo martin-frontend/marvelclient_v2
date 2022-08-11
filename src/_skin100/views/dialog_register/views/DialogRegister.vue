966406e36ee13f55d5baa5541956510178713e56
pangpang <afanso86@yandex.com>
2022-08-11 23:07:27 +0400

    1.bug修改

--- src/_skin100/views/dialog_register/views/DialogRegister.vue
+++ src/_skin100/views/dialog_register/views/DialogRegister.vue
@@ -3,8 +3,8 @@
 
 <script lang="ts">
 import AdvertiseInLogin from "@/views/widget/advertise_in_login/AdvertiseInLogin.vue";
-import CustomInput from "@/_skin100/views/widget/custom_input/CustomInput.vue";
-import CustomPassword from "@/_skin100/views/widget/custom_password/CustomPassword.vue";
+import CustomInput from "@/_skin100/views/widget/custom_input_pic/CustomInput.vue";
+import CustomPassword from "@/_skin100/views/widget/custom_password_pic/CustomPassword.vue";
 import Overlay from "@/_skin100/views/widget/overlay/Overlay.vue";
 import Component from "vue-class-component";
 import DialogRegister from "./DialogRegister";

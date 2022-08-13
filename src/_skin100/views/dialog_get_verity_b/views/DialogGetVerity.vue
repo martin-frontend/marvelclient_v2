973099c41d5888d9765790ee2f33f11a23218709
pangpang <afanso86@yandex.com>
2022-08-13 15:57:58 +0400

    1.支付密码设置

--- src/_skin100/views/dialog_get_verity_b/views/DialogGetVerity.vue
+++ src/_skin100/views/dialog_get_verity_b/views/DialogGetVerity.vue
@@ -2,8 +2,8 @@
 <style src="./DialogGetVerity.vue.scss" lang="scss" scoped></style>
 
 <script lang="ts">
-import CustomInput from "@/views/widget/custom_input/CustomInput.vue";
-import Overlay from "@/views/widget/overlay/Overlay.vue";
+import CustomInput from "@/_skin100/views/widget/custom_input/CustomInput.vue";
+import Overlay from "@/_skin100/views/widget/overlay/Overlay.vue";
 import Component from "vue-class-component";
 import DialogGetVerity from "./DialogGetVerity";
 

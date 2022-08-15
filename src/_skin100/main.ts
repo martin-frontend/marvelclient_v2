a0f6c1d530b439853410fc7853ed8220b9eab5cb
custer <custer@xx.xx>
2022-08-15 17:36:43 +0700

    测试环境下，url传plat_id channel_id

--- src/_skin100/main.ts
+++ src/_skin100/main.ts
@@ -6,16 +6,27 @@ import "@mdi/font/css/materialdesignicons.css";
 import "@/assets/iconfont/iconfont.css";
 import "@/_skin100/style/common.scss";
 import AppFacade from "@/AppFacade";
+import VueLoadmore from "vuejs-loadmore";
 import { getVuetify } from "./plugins/vuetify";
+import LogUtil from "@/core/global/LogUtil";
+import { js_utils } from "custer-js-utils";
 
+LogUtil.init();
 core.init();
-core.host = "http://api.starsabc.com/";
-core.cdnUrl = "http://sftpuser.starsabc.com/";
-core.plat_id = "30000";
-core.channel_id = "30000001";
+//@ts-ignore
+core.plat_id = core.channel_id = undefined;
+if (process.env.NODE_ENV == "production" && process.env.VUE_APP_ENV != "h5") {
+    core.cdnUrl = process.env.VUE_APP_URL;
+} else {
+    core.host = "http://api.starsabc.com/";
+    core.cdnUrl = "http://sftpuser.starsabc.com";
+    core.plat_id = js_utils.getQueryVariable("plat_id") || "30012";
+    core.channel_id = js_utils.getQueryVariable("channel_id") || "30012001";
+}
 AppFacade.inst.startup();
 
 Vue.config.productionTip = false;
+Vue.use(VueLoadmore);
 
 const vuetify =  getVuetify();
 //@ts-ignore

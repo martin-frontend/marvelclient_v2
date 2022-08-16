9678eb77d4ad8abf95b8c0ebec32f7c9e43442e6
custer <custer@xx.xx>
2022-08-16 15:37:12 +0700

    dialog_message_box bug

--- src/_skin100/main.ts
+++ src/_skin100/main.ts
@@ -5,11 +5,11 @@ import router from "./router";
 import "@mdi/font/css/materialdesignicons.css";
 import "@/assets/iconfont/iconfont.css";
 import "@/_skin100/style/common.scss";
-import AppFacade from "@/AppFacade";
 import VueLoadmore from "vuejs-loadmore";
 import { getVuetify } from "./plugins/vuetify";
 import LogUtil from "@/core/global/LogUtil";
 import { js_utils } from "custer-js-utils";
+import AppFacade from "./AppFacade";
 
 LogUtil.init();
 core.init();

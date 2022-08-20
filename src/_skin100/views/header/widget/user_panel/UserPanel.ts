948e3d44103fdea94a93f758e978fe4a40ea343d
pangpang <afanso86@yandex.com>
2022-08-20 22:43:40 +0400

    1.BUG解决

--- src/_skin100/views/header/widget/user_panel/UserPanel.ts
+++ src/_skin100/views/header/widget/user_panel/UserPanel.ts
@@ -17,6 +17,23 @@ import LangUtil from "@/core/global/LangUtil";
 import GamePlatConfig from "@/core/config/GamePlatConfig";
 import dialog_user_center from "@/_skin100/views/dialog_user_center";
 
+import aqzx from "@/_skin100/assets/my/aqzx.png";
+import aqzx_w from "@/_skin100/assets/my/hover/aqzx.png";
+import ptqb from "@/_skin100/assets/my/ptqb.png";
+import ptqb_w from "@/_skin100/assets/my/hover/ptqb.png";
+import tzjl from "@/_skin100/assets/my/tzjl.png";
+import tzjl_w from "@/_skin100/assets/my/hover/tzjl.png";
+import xxzx from "@/_skin100/assets/my/xxzx.png";
+import xxzx_w from "@/_skin100/assets/my/hover/xxzx.png";
+import tgzq from "@/_skin100/assets/my/tgzq.png";
+import tgzq_w from "@/_skin100/assets/my/hover/tgzq.png";
+import zsfh from "@/_skin100/assets/my/zsfh.png";
+import zsfh_w from "@/_skin100/assets/my/hover/zsfh.png";
+import yxwk from "@/_skin100/assets/my/yxwk.png";
+import yxwk_w from "@/_skin100/assets/my/hover/yxwk.png";
+import jchd from "@/_skin100/assets/my/jchd.png";
+import jchd_w from "@/_skin100/assets/my/hover/jchd.png";
+
 @Component
 export default class UserPanel extends AbstractView {
     LangUtil = LangUtil;
@@ -38,59 +55,113 @@ export default class UserPanel extends AbstractView {
         {
             id: 0,
             name: LangUtil("个人中心"),
-            icon: require("@/_skin100/assets/my/aqzx.png"),
-            icon_white: require("@/_skin100/assets/my/hover/aqzx.png"),
+            icon: aqzx,
+            icon_white: aqzx_w,
         },
         {
             id: 1,
             name: LangUtil("安全中心"),
-            icon: require("@/_skin100/assets/my/aqzx.png"),
-            icon_white: require("@/_skin100/assets/my/hover/aqzx.png"),
+            icon: aqzx,
+            icon_white: aqzx_w,
         },
         {
             id: 2,
             name: LangUtil("平台钱包"),
-            icon: require("@/_skin100/assets/my/ptqb.png"),
-            icon_white: require("@/_skin100/assets/my/hover/ptqb.png"),
+            icon: ptqb,
+            icon_white: ptqb_w,
         },
         {
             id: 3,
             name: LangUtil("投注记录"),
-            icon: require("@/_skin100/assets/my/tzjl.png"),
-            icon_white: require("@/_skin100/assets/my/hover/tzjl.png"),
+            icon: tzjl,
+            icon_white: tzjl_w,
         },
         {
             id: 4,
             name: LangUtil("消息中心"),
-            icon: require("@/_skin100/assets/my/xxzx.png"),
-            icon_white: require("@/_skin100/assets/my/hover/xxzx.png"),
+            icon: xxzx,
+            icon_white: xxzx_w,
         },
+        // {
+        //     id: 0,
+        //     name: LangUtil("个人中心"),
+        //     icon: require("@/_skin100/assets/my/aqzx.png"),
+        //     icon_white: require("@/_skin100/assets/my/hover/aqzx.png"),
+        // },
+        // {
+        //     id: 1,
+        //     name: LangUtil("安全中心"),
+        //     icon: require("@/_skin100/assets/my/aqzx.png"),
+        //     icon_white: require("@/_skin100/assets/my/hover/aqzx.png"),
+        // },
+        // {
+        //     id: 2,
+        //     name: LangUtil("平台钱包"),
+        //     icon: require("@/_skin100/assets/my/ptqb.png"),
+        //     icon_white: require("@/_skin100/assets/my/hover/ptqb.png"),
+        // },
+        // {
+        //     id: 3,
+        //     name: LangUtil("投注记录"),
+        //     icon: require("@/_skin100/assets/my/tzjl.png"),
+        //     icon_white: require("@/_skin100/assets/my/hover/tzjl.png"),
+        // },
+        // {
+        //     id: 4,
+        //     name: LangUtil("消息中心"),
+        //     icon: require("@/_skin100/assets/my/xxzx.png"),
+        //     icon_white: require("@/_skin100/assets/my/hover/xxzx.png"),
+        // },
     ];
     menuList1 = [
         {
             id: 10,
             name: LangUtil("推广赚钱"),
-            icon: require("@/_skin100/assets/my/tgzq.png"),
-            icon_white: require("@/_skin100/assets/my/hover/tgzq.png"),
+            icon: tgzq,
+            icon_white: tgzq_w,
         },
         {
             id: 11,
             name: LangUtil("终身分红"),
-            icon: require("@/_skin100/assets/my/zsfh.png"),
-            icon_white: require("@/_skin100/assets/my/hover/zsfh.png"),
+            icon: zsfh,
+            icon_white: zsfh_w,
         },
         {
             id: 12,
             name: LangUtil("游戏挖矿"),
-            icon: require("@/_skin100/assets/my/yxwk.png"),
-            icon_white: require("@/_skin100/assets/my/hover/yxwk.png"),
+            icon: yxwk,
+            icon_white: yxwk_w,
         },
         {
             id: 13,
             name: LangUtil("精彩活动"),
-            icon: require("@/_skin100/assets/my/jchd.png"),
-            icon_white: require("@/_skin100/assets/my/hover/jchd.png"),
+            icon: jchd,
+            icon_white: jchd_w,
         },
+        // {
+        //     id: 10,
+        //     name: LangUtil("推广赚钱"),
+        //     icon: require("@/_skin100/assets/my/tgzq.png"),
+        //     icon_white: require("@/_skin100/assets/my/hover/tgzq.png"),
+        // },
+        // {
+        //     id: 11,
+        //     name: LangUtil("终身分红"),
+        //     icon: require("@/_skin100/assets/my/zsfh.png"),
+        //     icon_white: require("@/_skin100/assets/my/hover/zsfh.png"),
+        // },
+        // {
+        //     id: 12,
+        //     name: LangUtil("游戏挖矿"),
+        //     icon: require("@/_skin100/assets/my/yxwk.png"),
+        //     icon_white: require("@/_skin100/assets/my/hover/yxwk.png"),
+        // },
+        // {
+        //     id: 13,
+        //     name: LangUtil("精彩活动"),
+        //     icon: require("@/_skin100/assets/my/jchd.png"),
+        //     icon_white: require("@/_skin100/assets/my/hover/jchd.png"),
+        // },
         // {
         //     id: 14,
         //     name: LangUtil("{0}币介绍", GamePlatConfig.getAwardCoin()),

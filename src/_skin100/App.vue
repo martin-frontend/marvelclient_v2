e06a68acaf2b72f02801b8b4d325fb48ff10274a
pangpang <afanso86@yandex.com>
2022-09-02 15:45:22 +0400

    1.bug解决

--- src/_skin100/App.vue
+++ src/_skin100/App.vue
@@ -10,6 +10,17 @@
         </div>
         <DialogMessage />
         <div id="dialog_container"></div>
+        <!-- 客服 -->
+        <v-btn
+            height="42"
+            color="#ffb01b"
+            class="customer rounded-xl black--text font-weight-bold d-flex align-center text-h6"
+            @click="onService"
+            v-if="!$vuetify.breakpoint.mobile"
+        >
+            <v-icon class="mr-1">mdi-message-text-outline</v-icon>
+            <span class="mb-1">{{ LangUtil("客服") }}</span>
+        </v-btn>
     </v-app>
 </template>
 
@@ -20,6 +31,7 @@ import Header from "@/_skin100/views/header/views/Header.vue";
 import Footer from "@/_skin100/views/footer/Footer.vue";
 import MobileMenu from "@/_skin100/views/mobile_menu/MobileMenu.vue";
 import DialogMessage from "@/_skin100/views/dialog_message/views/DialogMessage.vue";
+import APP from "./App";
 
 @Component({
     components: {
@@ -29,5 +41,21 @@ import DialogMessage from "@/_skin100/views/dialog_message/views/DialogMessage.v
         MobileMenu,
     },
 })
-export default class APP extends Vue {}
+export default class extends APP {}
 </script>
+
+<style lang="scss" scoped>
+.customer {
+    z-index: 100;
+    position: fixed;
+    right: 55px;
+    bottom: 60px;
+    z-index: 100;
+}
+.btn-guide {
+    width: 100%;
+    text-align: center;
+    position: fixed;
+    bottom: 90px;
+}
+</style>

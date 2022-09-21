5e189f434cbbe9673b0b92426fe7a74d7a8b0581
pangpang <afanso86@yandex.com>
2022-09-21 22:51:08 +0400

    fix bug

--- src/_skin100/views/widget/custom_input/CustomInput.ts
+++ src/_skin100/views/widget/custom_input/CustomInput.ts
@@ -8,6 +8,7 @@ export default class CustomInput extends AbstractView {
     @Prop({ default: "text" }) type!: string;
     @Prop({ default: 100 }) maxlength!: number;
     @Prop({ default: 100 }) diglength!: number; //小数位
+    @Prop({ default: 100 }) intlength!: number; //小数位
     @Prop({ default: false }) isNum!: boolean; //小数位
 
     inputValue = this.getValue;
@@ -20,7 +21,8 @@ export default class CustomInput extends AbstractView {
 
     onInput(value: any) {
         if (this.isNum) {
-            const RegStr = `^[\\+\\-]?\\d+\\.?\\d{0,${this.diglength}}`;
+            // const RegStr = `^[\\+\\-]?\\d+\\.?\\d{0,${this.diglength}}`;
+            const RegStr = `^[\\+\\-]?\\d{1,${this.intlength}}(\\.\\d{0,${this.diglength}})?`;
             this.inputValue = this.inputValue.match(new RegExp(RegStr, "g")) || null;
         }
         this.$emit("input", this.inputValue);

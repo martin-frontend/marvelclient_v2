84e0db977e40b0229179fe1823a34221c19fe6b3
custer <custer@xx.xx>
2022-10-21 15:41:39 +0700

    IO错误提示，忽略轮询接口

--- src/core/command/IOErrorCMD.ts
+++ src/core/command/IOErrorCMD.ts
@@ -3,6 +3,10 @@ import LangUtil from "../global/LangUtil";
 
 export default class IOErrorCMD extends puremvc.SimpleCommand {
     execute(notification: puremvc.INotification) {
-        dialog_message.error(LangUtil("服务器无法访问，请检查网络"));
+        const body = notification.getBody();
+        const arr = ["api/vendor/96/products", "api_test_speed", `api/user/${core.user_id}/red_dot_tips`, `api/user/show/${core.user_id}`, `api/plat/${core.plat_id}/recently_bet_info`];
+        if(!arr.includes(body.url)){
+            dialog_message.error(LangUtil("服务器无法访问，请检查网络"));
+        }
     }
 }

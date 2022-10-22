bb3e0f8ebe23a96ef4b1136a074854a26fe79f15
wayne <wayne@h5.com>
2022-10-22 17:13:39 +0700

    loadmore 语系

--- src/views/dialog_email/views/DialogEmail.vue
+++ src/views/dialog_email/views/DialogEmail.vue
@@ -6,11 +6,13 @@ import Component from "vue-class-component";
 import Overlay from "@/views/widget/overlay/Overlay.vue";
 import DialogEmail from "./DialogEmail";
 import ListNodata from "@/views/widget/list_nodata/ListNodata.vue";
+import LoadMore from "@/views/widget/loadMore/LoadMore.vue";
 
 @Component({
     components: {
         Overlay,
         ListNodata,
+        LoadMore,
     },
 })
 export default class extends DialogEmail {}
--- src/views/dialog_record_exchange/views/DialogRecordExchange.vue
+++ src/views/dialog_record_exchange/views/DialogRecordExchange.vue
@@ -6,11 +6,13 @@ import Overlay from "@/views/widget/overlay/Overlay.vue";
 import Component from "vue-class-component";
 import DialogRecordExchange from "./DialogRecordExchange";
 import ListNodata from "@/views/widget/list_nodata/ListNodata.vue";
+import LoadMore from "@/views/widget/loadMore/LoadMore.vue";
 
 @Component({
     components: {
         Overlay,
         ListNodata,
+        LoadMore,
     },
 })
 export default class extends DialogRecordExchange {}

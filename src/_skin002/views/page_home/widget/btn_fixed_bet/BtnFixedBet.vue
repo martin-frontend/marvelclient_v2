d3e2ea2e59fe2efcec0a4dac022462a31e7ffc30
martin <Martin.yu@baiyitech.com.tw>
2022-10-22 11:47:02 +0700

    init skin002

---
+++ src/_skin002/views/page_home/widget/btn_fixed_bet/BtnFixedBet.vue
@@ -0,0 +1,10 @@
+<template src="./BtnFixedBet.vue.html" lang="html"></template>
+<style src="./BtnFixedBet.vue.scss" lang="scss" scoped></style>
+
+<script lang="ts">
+import Component from "vue-class-component";
+import BtnFixedBet from "@/_skin001/views/page_home/widget/btn_fixed_bet/BtnFixedBet";
+
+@Component
+export default class extends BtnFixedBet {}
+</script>

ab8719b107b22c7135e28576cad1f450def723af
martin <Martin.yu@baiyitech.com.tw>
2022-10-28 12:11:54 +0800

    移除业绩查询弹窗的拥金栏位

---
+++ src/_skin001/views/dialog_performance/index.ts
@@ -0,0 +1,12 @@
+import DialogMount from "@/core/global/DialogMount";
+import getProxy from "@/core/global/getProxy";
+import DialogPerformanceProxy from "@/views/dialog_performance/proxy/DialogPerformanceProxy";
+import DialogPerformance from "./views/DialogPerformance.vue";
+
+function show() {
+    DialogMount(DialogPerformance);
+    const proxy: DialogPerformanceProxy = getProxy(DialogPerformanceProxy);
+    proxy.pageData.bShow = true;
+}
+
+export default { show };
---
+++ src/_skin001/views/dialog_performance/views/DialogPerformance.vue
@@ -0,0 +1,19 @@
+<template src="./DialogPerformance.vue.html" lang="html"></template>
+<style src="./DialogPerformance.vue.scss" lang="scss" scoped></style>
+
+<script lang="ts">
+import Component from "vue-class-component";
+import Overlay from "@/views/widget/overlay/Overlay.vue";
+import DialogPerformance from "./DialogPerformance";
+import CustomSelect from "@/views/widget/custom_select/CustomSelect.vue";
+import ListNodata from "@/views/widget/list_nodata/ListNodata.vue";
+
+@Component({
+    components: {
+        CustomSelect,
+        Overlay,
+        ListNodata,
+    },
+})
+export default class extends DialogPerformance {}
+</script>

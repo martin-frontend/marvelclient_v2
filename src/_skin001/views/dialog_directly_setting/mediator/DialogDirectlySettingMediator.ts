17c51e43ca9d45aad1a7485bc4ea162e07088913
nico <nico@nicodeMacBook-Pro.local>
2022-11-08 00:23:43 +0700

    修改直属扣款之后不刷新的问题

--- src/_skin001/views/dialog_directly_setting/mediator/DialogDirectlySettingMediator.ts
+++ src/_skin001/views/dialog_directly_setting/mediator/DialogDirectlySettingMediator.ts
@@ -3,16 +3,16 @@ import DialogDirectlySettingProxy from "../proxy/DialogDirectlySettingProxy";
 import getProxy from "@/core/global/getProxy";
 import DialogDirectlyProxy from "@/_skin001/views/dialog_directly/proxy/DialogDirectlyProxy";
 
-export default class DialogDirectlySettingMediator extends AbstractMediator{
+export default class DialogDirectlySettingMediator extends AbstractMediator {
     public listNotificationInterests(): string[] {
-        return [net.EventType.api_user_var_fetch_direct_user_info ,net.EventType.api_user_var_agent_direct_user_update ];
+        return [net.EventType.api_user_var_fetch_direct_user_info, net.EventType.api_user_var_agent_direct_user_update, net.EventType.api_user_var_agent_direct_deduction];
     }
 
     public handleNotification(notification: puremvc.INotification): void {
         const body = notification.getBody();
-        const myProxy:DialogDirectlySettingProxy = getProxy(DialogDirectlySettingProxy);
-        const dialogDirectly :DialogDirectlyProxy = getProxy(DialogDirectlyProxy);
-        
+        const myProxy: DialogDirectlySettingProxy = getProxy(DialogDirectlySettingProxy);
+        const dialogDirectly: DialogDirectlyProxy = getProxy(DialogDirectlyProxy);
+
         switch (notification.getName()) {
             case net.EventType.api_user_var_fetch_direct_user_info: //查询 用户信息
                 myProxy.setData(body);
@@ -21,6 +21,9 @@ export default class DialogDirectlySettingMediator extends AbstractMediator{
                 myProxy.pageData.loading = false;
                 dialogDirectly.api_user_var_agent_direct_list();
                 //console.log(" 收到刷新 下属 信息" ,body);
+                break;
+            case net.EventType.api_user_var_agent_direct_deduction: //扣款的回调
+                myProxy.api_user_var_fetch_direct_user_info(myProxy.playerInfo.user_id);
                 break;
         }
     }
--- src/_skin001/views/dialog_directly_transfer/mediator/DialogDirectlyTransferMediator.ts
+++ src/_skin001/views/dialog_directly_transfer/mediator/DialogDirectlyTransferMediator.ts
@@ -10,10 +10,10 @@ export default class DialogDirectlyTransferMediator extends AbstractMediator{
     public handleNotification(notification: puremvc.INotification): void {
         const body = notification.getBody();
         const myProxy:DialogDirectlyTransferProxy = getProxy(DialogDirectlyTransferProxy);
-
         switch (notification.getName()) {
             case net.EventType.api_user_var_agent_direct_deduction: //扣款的回调
                 myProxy.agent_direct_deduction_call_back(body);
+
                 break;
         }
 

2c1eb58b3e38d327622f60fd850816ce23ac4110
nico <nico@nicodeMacBook-Pro.local>
2022-11-07 18:26:11 +0700

    ty-892,修改，优化

---
+++ src/_skin001/views/dialog_directly_setting/mediator/DialogDirectlySettingMediator.ts
@@ -0,0 +1,27 @@
+import AbstractMediator from "@/core/abstract/AbstractMediator";
+import DialogDirectlySettingProxy from "../proxy/DialogDirectlySettingProxy";
+import getProxy from "@/core/global/getProxy";
+import DialogDirectlyProxy from "@/_skin001/views/dialog_directly/proxy/DialogDirectlyProxy";
+
+export default class DialogDirectlySettingMediator extends AbstractMediator{
+    public listNotificationInterests(): string[] {
+        return [net.EventType.api_user_var_fetch_direct_user_info ,net.EventType.api_user_var_agent_direct_user_update ];
+    }
+
+    public handleNotification(notification: puremvc.INotification): void {
+        const body = notification.getBody();
+        const myProxy:DialogDirectlySettingProxy = getProxy(DialogDirectlySettingProxy);
+        const dialogDirectly :DialogDirectlyProxy = getProxy(DialogDirectlyProxy);
+        
+        switch (notification.getName()) {
+            case net.EventType.api_user_var_fetch_direct_user_info: //查询 用户信息
+                myProxy.setData(body);
+                break;
+            case net.EventType.api_user_var_agent_direct_user_update: //刷新 用户信息
+                myProxy.pageData.loading = false;
+                dialogDirectly.api_user_var_agent_direct_list();
+                //console.log(" 收到刷新 下属 信息" ,body);
+                break;
+        }
+    }
+}

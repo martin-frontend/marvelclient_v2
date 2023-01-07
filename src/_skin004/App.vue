9254007d31b4b80eb8e5b81ce25e5397b248ec99
nico <nico@nicodeMacBook-Pro.local>
2023-01-07 17:26:36 +0700

    修改 主页 客服 公告字体大小

--- src/_skin004/App.vue
+++ src/_skin004/App.vue
@@ -40,21 +40,21 @@
         </v-btn> -->
         <v-sheet class="customer  d-flex flex-column" color="colorPanelTitleBg" elevation="0">
 
-            <v-btn min-height="110" color="transparent" class=" info--text text-16 " @click="onNoticeShow"
+            <v-btn min-height="110" color="transparent" class=" info--text" @click="onNoticeShow"
                 v-if="!$vuetify.breakpoint.mobile" elevation="0">
                 <div class="d-flex flex-column align-center mt-2">
                     <v-img class=" " src="@/_skin004/assets/notice.png" max-width="40"></v-img>
                     <!-- <v-icon class="mr-1">mdi-message-text-outline</v-icon> -->
-                    <div class="mt-2 item">{{ LangUtil("公告") }}</div>
+                    <div class="mt-2 item text-12">{{ LangUtil("公告") }}</div>
                 </div>
             </v-btn>
             <div class="ml-2 mr-2 hackhome"></div>
-            <v-btn min-height="110" color="transparent" class=" info--text text-16 " @click="onService"
+            <v-btn min-height="110" color="transparent" class=" info--text " @click="onService"
                 v-if="!$vuetify.breakpoint.mobile" elevation="0">
                 <div class="d-flex flex-column align-center mt-2">
                     <v-img src="@/_skin004/assets/kefu.png" max-width="40"></v-img>
                     <!-- <v-icon class="mr-1">mdi-message-text-outline</v-icon> -->
-                    <div class="mt-2 item">{{ LangUtil("客服") }}</div>
+                    <div class="mt-2 item text-12">{{ LangUtil("客服") }}</div>
                 </div>
             </v-btn>
         </v-sheet>>

40beb988943beba13f0d00f1e3dc4c23e5d4e03c
nico <nico@nicodeMacBook-Pro.local>
2023-02-02 21:49:28 +0700

    更换   条款内容

--- src/_skin003/views/footer/Footer.ts
+++ src/_skin003/views/footer/Footer.ts
@@ -32,19 +32,23 @@ export default class Footer extends AbstractView {
     goService_2(tag:number) {
         if (tag == 1)
         {
-            dialog_service.show(LangUtil("AML政策标题") , LangUtil("AML政策内容"));
+            //dialog_service.show(LangUtil("AML政策标题") , LangUtil("AML政策内容"));
+            OpenLink(LangUtil("white/pdf/AML_Policy.pdf"));
         }
         else if (tag == 2)
         {
-            dialog_service.show(LangUtil("隐私政策标题") , LangUtil("隐私政策内容"));
+            OpenLink(LangUtil("white/pdf/Privacy_Policy.pdf"));
+            //dialog_service.show(LangUtil("隐私政策标题") , LangUtil("隐私政策内容"));
         }
         else if (tag == 3)
         {
-            dialog_service.show(LangUtil("负责任的赌博标题") , LangUtil("负责任的赌博内容"));
+            OpenLink(LangUtil("white/pdf/Responsible_Gambling.pdf"));
+            //dialog_service.show(LangUtil("负责任的赌博标题") , LangUtil("负责任的赌博内容"));
         }
         else if (tag == 4)
         {
-            dialog_service.show(LangUtil("条款和条件标题") , LangUtil("条款和条件内容"));
+            OpenLink(LangUtil("white/pdf/Terms_and_Conditions.pdf"));
+            //dialog_service.show(LangUtil("条款和条件标题") , LangUtil("条款和条件内容"));
         }
     }
 }

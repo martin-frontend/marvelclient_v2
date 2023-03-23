c311a7b543c55069255fb48664418ccd3e1490c0
custer <custer@xx.xx>
2023-03-23 11:36:20 +0900

    FB体育不刷新的问题

--- src/_skin004/views/page_game_play/index.ts
+++ src/_skin004/views/page_game_play/index.ts
@@ -1,11 +1,18 @@
 import getProxy from "@/core/global/getProxy";
 import PageGamePlayProxy from "./proxy/PageGamePlayProxy";
 
-function show(url: string) {
+function show(url: string, bWait: boolean = false) {
     //@ts-ignore
     window["vm"].$router.push("/page_game_play");
     const proxy: PageGamePlayProxy = getProxy(PageGamePlayProxy);
-    proxy.pageData.url = url;
+    if (bWait) {
+        proxy.pageData.url = "";
+        setTimeout(() => {
+            proxy.pageData.url = url;
+        }, 200);
+    } else {
+        proxy.pageData.url = url;
+    }
 }
 
 export default { show };

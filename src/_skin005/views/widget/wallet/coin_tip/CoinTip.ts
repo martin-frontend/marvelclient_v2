e66840c3ca61bf2776ab63e8e16f35aaf0b93192
nico <nico@nicodeMacBook-Pro.local>
2023-04-04 19:38:56 +0900

    修改 手机版 当前币种tips 不正确的问题

--- src/_skin005/views/widget/wallet/coin_tip/CoinTip.ts
+++ src/_skin005/views/widget/wallet/coin_tip/CoinTip.ts
@@ -6,4 +6,25 @@ import { Prop, Watch, Component } from "vue-property-decorator";
 export default class CoinTip extends AbstractView {
     LangUtil = LangUtil;
     @Prop({ default: true }) isLeft!: boolean;
+
+    get tipsClass(): string {
+        let _class = "";
+        if (this.$vuetify.breakpoint.xsOnly) {
+            _class = _class + " tip_color_mob";
+        } else {
+            _class = _class + " tip_color_pc";
+        }
+
+        if (this.isLeft) {
+            if (this.$vuetify.breakpoint.xsOnly) {
+                _class = _class + " tip_left_mob";
+            } else {
+                _class = _class + " tip_left";
+            }
+        } else {
+            _class = _class + " tip_right";
+        }
+
+        return _class;
+    }
 }

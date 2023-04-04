a70f1f8cbdfd9a9f27af91397ddebd18ed220b12
nico <nico@nicodeMacBook-Pro.local>
2023-04-04 21:28:07 +0900

    小修改

--- src/_skin005/core/ModulesHelper.ts
+++ src/_skin005/core/ModulesHelper.ts
@@ -1,108 +1,112 @@
 import GameConfig from "@/core/config/GameConfig";
 import PanelUtil from "./PanelUtil";
 
-function _isValueTrue(str:string) {
-
-    if (GameConfig && GameConfig.config && GameConfig.config.modules_switch && GameConfig.config.modules_switch[str] == 0)
-    {
-        return false
+function _isValueTrue(str: string, isDefault: boolean = true) {
+    if (isDefault) {
+        if (GameConfig && GameConfig.config && GameConfig.config.modules_switch && GameConfig.config.modules_switch[str] == 0) {
+            return false;
+        }
+        return true;
+    } else {
+        if (GameConfig && GameConfig.config && GameConfig.config.modules_switch && GameConfig.config.modules_switch[str] == 1) {
+            return true;
+        }
+        return false;
     }
-    return true;
 }
 
 /**
  * 是否显示 VIP信息
  */
 function IsShow_VipInfo() {
-    return _isValueTrue("gameWater")
+    return _isValueTrue("gameWater");
 }
 
 /**
  * 是否显示 币种介绍
  */
 function IsShow_CoinIntroduce() {
-    return _isValueTrue("coinIntroduce")
+    return _isValueTrue("coinIntroduce");
 }
 
 /**
  * 是否显示 质押分红
  */
 function IsShow_PledgeDividend() {
-    return _isValueTrue("pledgeDividend")
+    return _isValueTrue("pledgeDividend");
 }
 
 /**
  * 是否显示 推广赚钱
  */
 function IsShow_Promotion() {
-    return _isValueTrue("promotion")
+    return _isValueTrue("promotion");
 }
 
 /**
  * 是否显示 游戏反水
  */
 function IsShow_GameWater() {
-    return _isValueTrue("gameWater")
+    return _isValueTrue("gameWater");
 }
 
 /**
  * 是否显示 swap交易
  */
 function IsShow_Swap() {
-    return _isValueTrue("swap")
+    return _isValueTrue("swap");
 }
 
 /**
  * 是否显示 精彩活动
  */
 function IsShow_ActivityDisplay() {
-    return _isValueTrue("activityDisplay")
+    return _isValueTrue("activityDisplay");
 }
 /**
  * 是否显示 顶部的足球 页签 以及 手机版 底部的页签
  */
 function IsShow_FootBall() {
-    return _isValueTrue("menuFootball")
+    return _isValueTrue("menuFootball");
 }
 /**
  * 是否显示 首页的足球热门赛事
  */
 function IsShow_FootBallHot() {
-    return _isValueTrue("footballHot")
+    return _isValueTrue("footballHot");
 }
 /**
  * 是否显示 首页返水的领取 gameWaterGuide
  */
 function IsShow_GameWaterManualReceive() {
-    return _isValueTrue("gameWaterManualReceive")
+    return _isValueTrue("gameWaterManualReceive");
 }
 /**
  * 是否显示 首页返水的引导 gameWaterGuide
  */
 function IsShow_GameWaterGuide() {
-    return _isValueTrue("gameWaterGuide")
+    return _isValueTrue("gameWaterGuide");
 }
 /**
  * 如果为1 则需要  手动计算 vip的返奖比例
  */
 function IsShow_VipShowDeal() {
-    return _isValueTrue("VipShowDeal")
+    return _isValueTrue("VipShowDeal", false);
 }
 /**
  * 是否显示 左侧导航栏 悬浮 样式是否显示
  */
 function IsShow_pcMenuTip() {
-    return _isValueTrue("pcMenuTip")
+    return _isValueTrue("pcMenuTip");
 }
 /**
  * //是否显示 代理管理
- * @returns 
+ * @returns
  */
 function IsShow_AgentManager() {
     //return IsShow_Directly() == 2;
 
-    if (IsShow_Directly() == 2)
-    {
+    if (IsShow_Directly() == 2) {
         const selfProxy = PanelUtil.getProxy_selfproxy;
         if (!(selfProxy && selfProxy.userInfo && selfProxy.userInfo.user_id != 0)) {
             return false;
@@ -116,13 +120,11 @@ function IsShow_AgentManager() {
     return false;
 }
 
-
 /**
  * 是否显示 代理管理
  * @returns //1-展示推广赚钱|2-展示代理管理|3-都不展示
  */
 function IsShow_Directly() {
-
     const selfProxy = PanelUtil.getProxy_selfproxy;
     if (!(selfProxy && selfProxy.userInfo && selfProxy.userInfo.user_id != 0)) {
         return 0;
@@ -150,14 +152,14 @@ function isShow_Fan_shui() {
 }
 /**
  * 是否显示客服
- * @returns 
+ * @returns
  */
 function isShow_Kefu() {
-    return _isValueTrue("sysService")
+    return _isValueTrue("sysService");
 }
 
 export default {
-    IsShow_VipInfo ,
+    IsShow_VipInfo,
     IsShow_CoinIntroduce,
     IsShow_PledgeDividend,
     IsShow_Promotion,
@@ -174,4 +176,4 @@ export default {
     IsShow_pcMenuTip,
     IsShow_GameWaterGuide,
     IsShow_VipShowDeal,
-}
+};

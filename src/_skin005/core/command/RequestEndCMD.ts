52849fe3ea284db0f72b9bc80776787fd72357c6
custer <custer@xx.xx>
2023-04-11 09:56:05 +0900

    RequestEndCMD 错误

--- src/_skin005/core/command/RequestEndCMD.ts
+++ src/_skin005/core/command/RequestEndCMD.ts
@@ -3,41 +3,42 @@ import { TrackEventMap, track } from "@/_skin005/core/TrackManager";
 
 export default class RequestEndCMD extends puremvc.SimpleCommand {
     execute(notification: puremvc.INotification) {
-        // GlobalVar.net_status.loading = false;
         const body = notification.getBody();
+        let json: any;
         try {
-            const json = JSON.parse(body.result);
+            json = JSON.parse(body.result);
             GlobalVar.server_time = json.extend.microtime >> 0;
         } catch (e: any) {
             console.log(body);
         }
 
         const { url, data } = body;
-        const result = JSON.parse(body.result);
-        switch (url) {
-            case net.HttpType.api_user_register:
-                {
-                    const { phone, email, username, user_id } = result.data;
-                    track(TrackEventMap.RegistrationSuccess, { phone, email, username, user_id });
-                }
-                break;
-            case net.HttpType.api_user_login:
-                {
-                    const { phone, email, username, user_id } = result.data;
-                    track(TrackEventMap.loginSuccess, { phone, email, username, user_id });
-                }
-                break;
-            case net.getUrl(net.HttpType.api_user_update_var, data):
-                if (data.real_name) {
-                    track(TrackEventMap.SetAccountName);
-                }
-                break;
-            case net.getUrl(net.HttpType.api_user_var_exchange_create_order, data):
-                track(TrackEventMap.withdrawalRequested, {
-                    amount: data.amount,
-                    coin_name_unique: data.coin_name_unique,
-                });
-                break;
+        if (json) {
+            switch (url) {
+                case net.HttpType.api_user_register:
+                    {
+                        const { phone, email, username, user_id } = json.data;
+                        track(TrackEventMap.RegistrationSuccess, { phone, email, username, user_id });
+                    }
+                    break;
+                case net.HttpType.api_user_login:
+                    {
+                        const { phone, email, username, user_id } = json.data;
+                        track(TrackEventMap.loginSuccess, { phone, email, username, user_id });
+                    }
+                    break;
+                case net.getUrl(net.HttpType.api_user_update_var, data):
+                    if (data.real_name) {
+                        track(TrackEventMap.SetAccountName);
+                    }
+                    break;
+                case net.getUrl(net.HttpType.api_user_var_exchange_create_order, data):
+                    track(TrackEventMap.withdrawalRequested, {
+                        amount: data.amount,
+                        coin_name_unique: data.coin_name_unique,
+                    });
+                    break;
+            }
         }
     }
 }

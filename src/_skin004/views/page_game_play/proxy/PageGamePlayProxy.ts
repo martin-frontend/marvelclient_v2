810840c3039c50b149997659f7bb186621f4b5d2
custer <custer@xx.xx>
2023-04-12 14:36:40 +0900

    game_play页面，刷新直接回首页

--- src/_skin004/views/page_game_play/proxy/PageGamePlayProxy.ts
+++ src/_skin004/views/page_game_play/proxy/PageGamePlayProxy.ts
@@ -1,5 +1,15 @@
+import Vue from "vue";
+
 export default class PageGamePlayProxy extends puremvc.Proxy {
     static NAME = "PageGamePlayProxy";
+
+    public onRegister(): void {
+        setTimeout(() => {
+            if (this.pageData.url == "") {
+                Vue.router.replace("/");
+            }
+        }, 500);
+    }
 
     pageData = {
         loading: false,

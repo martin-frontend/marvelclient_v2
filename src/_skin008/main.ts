01a4535e40684f1efaf69c9f74b9601cff0b36a3
custer <custer@xx.xx>
2023-04-26 20:23:48 +0900

    skin008 图标

--- src/_skin008/main.ts
+++ src/_skin008/main.ts
@@ -38,6 +38,7 @@ import Footer from "@/_skin008/views/footer/Footer.vue";
 Assets.commonIcon.loading_img = "loding_icon_8.png";
 Assets.commonIcon.logo = require(`@/_skin008/assets/logo.png`);
 Assets.commonIcon.logo_m = require(`@/_skin008/assets/logo_m.png`);
+Assets.commonIcon.loading_img = "loding_icon_8.png";
 
 //SkinVariable.isShowFootDetail = false;
 //SkinVariable.loadingType = "006";
@@ -117,5 +118,6 @@ setInterval(() => {
         const isDark = window["vm"].$vuetify.theme.isDark;
         Assets.commonIcon.logo = isDark ? require(`@/_skin008/assets/logo.png`) : require(`@/_skin008/assets/logo_light.png`);
         Assets.commonIcon.logo_m = isDark ? require(`@/_skin008/assets/logo_m.png`) : require(`@/_skin008/assets/logo_m_light.png`);
+        Assets.commonIcon.loading_img = isDark ? "loding_icon_8.png" : "loding_icon_8_light.png";
     }
 }, 100);

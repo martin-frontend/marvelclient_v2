bdc44f48a38535902385516a81b50d2d67379dfe
nico <nico@nicodeMacBook-Pro.local>
2023-05-03 11:10:13 +0900

    修改推广赚钱 今日 数据不正确的问题

--- src/_skin005/views/page_extension/mediator/PageExtensionMediator.ts
+++ src/_skin005/views/page_extension/mediator/PageExtensionMediator.ts
@@ -48,9 +48,9 @@ export default class PageExtensionMediator extends AbstractMediator {
                 // this.sendNotification(net.HttpType.api_user_var_short_chain, { user_id: core.user_id });
                 this.myProxy.api_user_var_short_chain();
                 console.log("-------extension-----send");
-                if (this.isToday(body.date)) {
+                //if (this.isToday(body.date)) {
                     this.myProxy.setData(body);
-                }
+                //}
                 break;
             case net.EventType.api_user_var_commission_commissionnum:
                 myProxy.setCommissionCommissionnum(body);
--- src/_skin005/views/page_extension/views/widget/fanyong/Fanyong.ts
+++ src/_skin005/views/page_extension/views/widget/fanyong/Fanyong.ts
@@ -19,7 +19,9 @@ export default class Fanyong extends AbstractView {
     }
 
     transformMoney_commission(val: any) {
-        const sss = val * CoinTransformHelper.GetMainCoinScale;
+        const coinMoney = val[CoinTransformHelper.platCoins.mainCoin.name] || 0;
+
+        const sss = coinMoney * CoinTransformHelper.GetMainCoinScale;
 
         if (!ModulesHelper.RebateDisplayType()) {
             return amountFormat(sss / 100, true) + this.LangUtil("%");

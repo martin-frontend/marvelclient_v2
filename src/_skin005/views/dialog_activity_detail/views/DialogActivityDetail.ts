54954a1ce68390207bb032d1f49102e630ee3c8c
nico <nico@gmail.com>
2023-05-09 10:18:01 +0000

    4577 富文本内部链接打开问题处理

--- src/_skin005/views/dialog_activity_detail/views/DialogActivityDetail.ts
+++ src/_skin005/views/dialog_activity_detail/views/DialogActivityDetail.ts
@@ -5,6 +5,7 @@ import PanelUtil from "@/_skin005/core/PanelUtil";
 import { Watch, Component } from "vue-property-decorator";
 import DialogActivityDetailMediator from "../mediator/DialogActivityDetailMediator";
 import DialogActivityDetailProxy from "../proxy/DialogActivityDetailProxy";
+import OpenLink from "@/core/global/OpenLink";
 
 @Component
 export default class DialogActivityDetail extends AbstractView {
@@ -23,8 +24,29 @@ export default class DialogActivityDetail extends AbstractView {
     @Watch("pageData.bShow")
     onWatchShow() {
         PageBlur.blur_page(this.pageData.bShow);
-        // if (!this.pageData.bShow) {
-        //     PanelUtil.openpanel_activity();
-        // }
+        if (this.pageData.bShow) {
+            this.$nextTick(() => {
+                const mainNode = document.getElementById("main_node");
+                if (mainNode) {
+                    //console.log("回到顶部");
+                    mainNode.scrollTop = 0;
+                }
+            });
+        }
+    }
+    handleLinkClick(event: any) {
+        // 检查点击的目标元素是否是超链接
+        if (event.target.tagName === "U" || event.target.tagName === "A") {
+            const targetLink = event.target.closest("a") || event.target.parentNode.closest("a");
+            if (targetLink) {
+                // 在这里执行您的点击事件处理逻辑
+                console.log("超链接被点击了", event.target);
+                // 取消默认的链接跳转行为
+                event.preventDefault();
+                const targetUrl = targetLink.href;
+                console.log("超链接被点击了，目标 URL:", targetUrl);
+                OpenLink(targetUrl);
+            }
+        }
     }
 }
--- src/_skin005/views/dialog_notice_detail/views/DialogNoticeDetail.ts
+++ src/_skin005/views/dialog_notice_detail/views/DialogNoticeDetail.ts
@@ -5,6 +5,7 @@ import DialogNoticeDetailProxy from "../proxy/DialogNoticeDetailProxy";
 import LangUtil from "@/core/global/LangUtil";
 import MultDialogManager from "@/_skin005/core/MultDialogManager";
 import PageBlur from "@/_skin005/core/PageBlur";
+import OpenLink from "@/core/global/OpenLink";
 
 @Component
 export default class DialogNoticeDetail extends AbstractView {
@@ -24,5 +25,30 @@ export default class DialogNoticeDetail extends AbstractView {
     @Watch("pageData.bShow")
     onWatchShow() {
         PageBlur.blur_page(this.pageData.bShow);
+        if (this.pageData.bShow) {
+            this.$nextTick(() => {
+                const mainNode = document.getElementById("main_node");
+                if (mainNode) {
+                    //console.log("回到顶部");
+                    mainNode.scrollTop = 0;
+                }
+            });
+        }
+    }
+
+    handleLinkClick(event: any) {
+        // 检查点击的目标元素是否是超链接
+        if (event.target.tagName === "U" || event.target.tagName === "A") {
+            const targetLink = event.target.closest("a") || event.target.parentNode.closest("a");
+            if (targetLink) {
+                // 在这里执行您的点击事件处理逻辑
+                console.log("超链接被点击了", event.target);
+                // 取消默认的链接跳转行为
+                event.preventDefault();
+                const targetUrl = targetLink.href;
+                console.log("超链接被点击了，目标 URL:", targetUrl);
+                OpenLink(targetUrl);
+            }
+        }
     }
 }

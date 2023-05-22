2ebdc5e11034ff5a241b5106de432715bdb6b7a3
martin <martin@gmail.com>
2023-05-22 10:12:29 +0000

    H5-4581 skin004_2调整

--- src/_skin004_2/views/page_extension/views/PageExtension.ts
+++ src/_skin004_2/views/page_extension/views/PageExtension.ts
@@ -4,7 +4,7 @@ import PageExtensionMediator from "@/_skin004/views/page_extension/mediator/Page
 import PageExtensionProxy from "@/_skin004/views/page_extension/proxy/PageExtensionProxy";
 import dialog_bind_invite from "@/_skin001/views/dialog_bind_invite";
 import dialog_directly from "@/_skin001/views/dialog_directly";
-import dialog_performance from "@/_skin001/views/dialog_performance/index";
+import dialog_performance from "@/_skin004/views/dialog_performance/index";
 import dialog_message from "@/views/dialog_message";
 import dialog_wallet from "@/_skin001/views/dialog_wallet";
 import dialog_message_box from "@/views/dialog_message_box";

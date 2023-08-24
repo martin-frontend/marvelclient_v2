2465ee8ed53b97d8c56b5abeea13c7a4cd1263ac
nico <nico@gmail.com>
2023-08-24 15:57:26 +0000

    新增转盘落地页

---
+++ public_landing/template/3/js/net.js
@@ -0,0 +1,53 @@
+//发送消息
+function sendPostMessage(data) {
+    if (window.parent) window.parent.postMessage(data, "*");
+}
+
+//获取配置信息
+window.addEventListener("message", (e) => {
+    switch (e.data.action) {
+        case "config":
+            window.config = e.data.params;
+            console.log(e.data);
+            init();
+            break;
+        case "api_public_area_code":
+            areaCodeArr = e.data.params;
+            areaCode(areaCodeArr);
+            break;
+        case "api_public_auth_code":
+            {
+                const imgCodes = document.getElementsByClassName("imgCode");
+                for (const imgCode of imgCodes) {
+                    imgCode.setAttribute("src", e.data.params);
+                }
+            }
+            break;
+        case "api_user_register":
+            $("#loading").css("display", "none");
+            $("#txtMsg").append(e.data.params.msg);
+            $("#dialog_confirm").css("display", "flex");
+            break;
+        case "register_succeed":
+            {
+                token = e.data.params.data.token;
+                user_id = e.data.params.data.user_id;
+                uuid = e.data.uuid;
+                $("#loading").css("display", "none");
+                sendPostMessage({ action: "go_home", params: { token: token, uuid: uuid, user_id: user_id } });
+                // $("#dialog_succeed").css("display", "flex");
+                // resetForm();
+                // sendPostMessage({ action: "auth_code" });
+            }
+            break;
+        case "api_public_sms_send":
+            $("#loading").css("display", "none");
+            $("#txtSMSMsg").append(e.data.params.msg);
+            $("#dialog_sms_confirm").css("display", "flex");
+            break;
+        case "sms_success":
+            $("#loading").css("display", "none");
+            beginCountdown();
+            break;
+    }
+});
---
+++ public_landing/template/3/js/str.js
@@ -0,0 +1,15 @@
+var str = {
+    invite_user_id_placeholder: "referrer's code (optional)",
+    username_placeholder: "Username",
+    username_error: "Please enter your username. between 4 and 20 characters.",
+    mobile_placeholder: "Mobile",
+    mobile_error: "Please enter your mobile.",
+    email_placeholder: "Email",
+    email_error: "Please enter your email.",
+    password_placeholder: "Password",
+    password_error: "Please enter your password. between 6 and 20 characters.",
+    password_confirm_placeholder: "Confirm Password",
+    password_confirm_error: "Please confirm your password.",
+    verify_code_placeholder: "Verification Code",
+    verify_code_error: "Please enter the Verification Code.",
+};

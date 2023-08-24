d10f94a1c1d65333193adb5c38509ecd14ba5aa6
nico <nico@gmail.com>
2023-08-24 15:46:48 +0000

    5179-板球顶端被遮挡的问题

--- src/_skin005/views/page_game_soccer/views/PageGameSoccer.ts
+++ src/_skin005/views/page_game_soccer/views/PageGameSoccer.ts
@@ -26,11 +26,11 @@ export default class PageGameSoccer extends AbstractView {
     }
 
     get isCricket() {
+        const curPath = this.$router.app.$route.path;
         if (!this.myProxy.pageData.other_data) {
-            const curPath = this.$router.app.$route.path;
             return curPath.includes("cricket");
         } else {
-            return this.myProxy.pageData.other_data && this.myProxy.pageData.other_data.is_show_head === 1;
+            return curPath.includes("cricket") || (this.myProxy.pageData.other_data && this.myProxy.pageData.other_data.is_show_head === 1);
             // return this.myProxy.pageData.other_data ;
         }
     }

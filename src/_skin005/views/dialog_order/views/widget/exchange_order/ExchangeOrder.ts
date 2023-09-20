b361c5505e5374299aeea9c06113b1ccd17e0e4c
martin <martin@gmail.com>
2023-09-20 07:20:34 +0000

    H5-5319

--- src/_skin005/views/dialog_order/views/widget/exchange_order/ExchangeOrder.ts
+++ src/_skin005/views/dialog_order/views/widget/exchange_order/ExchangeOrder.ts
@@ -19,6 +19,9 @@ export default class ExchangeOrder extends AbstractView {
         return price;
     }
     getDate(date: any) {
+        if (date === 0) {
+            return "-";
+        }
         return changeDateShow(Timezone.Instance.convertTime_to_Locale_utc(Number(date)), true, true);
     }
     //根据盘口展示已结算的赛果角球还是比分等

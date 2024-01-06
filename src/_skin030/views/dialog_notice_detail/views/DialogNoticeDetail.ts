828491ba31bffc230b514933ba7a5cc94cab9ea7
nico <nico@gmail.com>
2024-01-06 01:20:48 +0000

    6330 富文本跳转问题

--- src/_skin030/views/dialog_notice_detail/views/DialogNoticeDetail.ts
+++ src/_skin030/views/dialog_notice_detail/views/DialogNoticeDetail.ts
@@ -35,20 +35,4 @@ export default class DialogNoticeDetail extends AbstractView {
             });
         }
     }
-
-    handleLinkClick(event: any) {
-        // 检查点击的目标元素是否是超链接
-        if (event.target.tagName === "U" || event.target.tagName === "A") {
-            const targetLink = event.target.closest("a") || event.target.parentNode.closest("a");
-            if (targetLink) {
-                // 在这里执行您的点击事件处理逻辑
-                console.log("超链接被点击了", event.target);
-                // 取消默认的链接跳转行为
-                event.preventDefault();
-                const targetUrl = targetLink.href;
-                console.log("超链接被点击了，目标 URL:", targetUrl);
-                OpenLink(targetUrl);
-            }
-        }
-    }
 }

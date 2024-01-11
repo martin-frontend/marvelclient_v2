8f8b3d27b45e7472a53407c0b51262ae70d44573
nico <nico@gmail.com>
2024-01-11 07:13:28 +0000

    016 富文本修改

--- src/_skin030/views/dialog_activity_detail/views/DialogActivityDetail.ts
+++ src/_skin030/views/dialog_activity_detail/views/DialogActivityDetail.ts
@@ -5,6 +5,7 @@ import { Watch, Component } from "vue-property-decorator";
 import DialogActivityDetailMediator from "../mediator/DialogActivityDetailMediator";
 import DialogActivityDetailProxy from "../proxy/DialogActivityDetailProxy";
 import OpenLink from "@/core/global/OpenLink";
+import PanelUtil from "@/_skin030/core/PanelUtil";
 
 @Component
 export default class DialogActivityDetail extends AbstractView {
@@ -31,6 +32,30 @@ export default class DialogActivityDetail extends AbstractView {
                     mainNode.scrollTop = 0;
                 }
             });
+        }
+    }
+    handleLinkClick(event: any) {
+        // 检查点击的目标元素是否是超链接
+        if (event.target.tagName === "U" || event.target.tagName === "A") {
+            const targetLink = event.target.closest("a") || event.target.parentNode.closest("a");
+            if (targetLink) {
+                // 取消默认的链接跳转行为
+                event.preventDefault();
+                const targetUrl = targetLink.href;
+                //提取targetLink中的原始的值
+                const targetUrlOrigin = targetLink.getAttribute("href");
+                const obj = {
+                    open_mode_url: targetUrlOrigin,
+                };
+                if (PanelUtil.isCanJump(obj)) {
+                    PanelUtil.jumpTo(obj);
+                    this.onClose();
+                } else {
+                    PanelUtil.jumpTo({
+                        open_mode_url: targetUrl,
+                    });
+                }
+            }
         }
     }
 }
--- src/_skin030/views/dialog_notice_detail/views/DialogNoticeDetail.ts
+++ src/_skin030/views/dialog_notice_detail/views/DialogNoticeDetail.ts
@@ -6,6 +6,7 @@ import LangUtil from "@/core/global/LangUtil";
 import MultDialogManager from "@/_skin030/core/MultDialogManager";
 import PageBlur from "@/_skin030/core/PageBlur";
 import OpenLink from "@/core/global/OpenLink";
+import PanelUtil from "@/_skin030/core/PanelUtil";
 
 @Component
 export default class DialogNoticeDetail extends AbstractView {
@@ -33,6 +34,30 @@ export default class DialogNoticeDetail extends AbstractView {
                     mainNode.scrollTop = 0;
                 }
             });
+        }
+    }
+    handleLinkClick(event: any) {
+        // 检查点击的目标元素是否是超链接
+        if (event.target.tagName === "U" || event.target.tagName === "A") {
+            const targetLink = event.target.closest("a") || event.target.parentNode.closest("a");
+            if (targetLink) {
+                // 取消默认的链接跳转行为
+                event.preventDefault();
+                const targetUrl = targetLink.href;
+                //提取targetLink中的原始的值
+                const targetUrlOrigin = targetLink.getAttribute("href");
+                const obj = {
+                    open_mode_url: targetUrlOrigin,
+                };
+                if (PanelUtil.isCanJump(obj)) {
+                    PanelUtil.jumpTo(obj);
+                    this.onClose();
+                } else {
+                    PanelUtil.jumpTo({
+                        open_mode_url: targetUrl,
+                    });
+                }
+            }
         }
     }
 }

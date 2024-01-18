3c890446cd948382502fccca7f9a4cbff77896fb
nico <nico@nicodeMacBook-Pro.local>
2024-01-18 11:59:18 +0900

    修改016皮手机版充值显示大小

--- src/_skin005/views/page_recharge/views/PageRecharge.ts
+++ src/_skin005/views/page_recharge/views/PageRecharge.ts
@@ -83,6 +83,6 @@ export default class PageRecharge extends AbstractView {
     }
 
     get isSkin008() {
-        return GlobalVar.skin == "skin008" || GlobalVar.skin == "skin010" || GlobalVar.skin == "skin012" || GlobalVar.skin == "skin017";
+        return GlobalVar.skin == "skin008" || GlobalVar.skin == "skin010" || GlobalVar.skin == "skin012" || GlobalVar.skin == "skin016" || GlobalVar.skin == "skin017";
     }
 }
